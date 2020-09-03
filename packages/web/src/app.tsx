72493b91337ec4ef10ac96a93f6066d356e9a7b0
James Salas <osdev@jamesbs.com>
2020-09-03 00:33:30 -0400

    add netlify, get web build running

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export const app = () => {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
